48cfa8d3202abd3208983768350e33680a28c291
Josiel Rocha <josiel.rocha@gmail.com>
2020-06-23 13:17:06 -0300

    Adiciona json_serde

--- src/main.rs
+++ src/main.rs
@@ -5,50 +5,82 @@ use std::io::BufReader;
 use std::io::prelude::*;
 
 use glob::glob;
-use serde_json::{Result, Value};
+use serde_json;
+use serde::{Deserialize, Serialize};
 
-fn export_har_file(path: String) {
-	let content = match read_file_content(path) {
-		Ok(contents) => contents,
-		Err(e) => e.to_string()
-	};
 
-	// println!("{}", content);
-	let data = match serde_json::from_str(&content).expect("Failed to read JSON data") {
-		Ok(content) => content
-	};
-
-	println!("{}", data);
+#[derive(Debug, Serialize, Deserialize)]
+struct HttpHeader {
+	name: String,
+	value: String
 }
 
-fn read_file_content(path: String) -> Result<String, std::io::Error> {
+#[derive(Debug, Serialize, Deserialize)]
+struct ResponseContent {
+	text: String
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Response {
+	headers: HttpHeader,
+	content: ResponseContent
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Request {
+	url: String
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Resource {
+	request: Request,
+	response: Response
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Log {
+	entries: Vec<Resource>
+}
+
+fn export_har_file(path: String) -> std::io::Result<()> {
+	let content = read_file_content(path)?;
+	let log: Log = serde_json::from_str(&content)?;
+	
+	for entry in log.entries {
+		println!("{}", 1)
+	}
+	
+	Ok(())
+}
+
+fn read_file_content(path: String) -> std::io::Result<String> {
 	println!("export_har_file({})", &path);
 
-	let file = match File::open(&path) {
-		Ok(file) => file,
-		Err(e) => return Err(e)
-	};
-
+	let file = File::open(&path)?;
 	let mut buf_reader = BufReader::new(file);
 	let mut contents = String::new();
 
+	println!("{}", "JOSIEL");
+
 	match buf_reader.read_to_string(&mut contents) {
 		Ok(_) => Ok(contents),
-		Err(e) => Err(e),
+		Err(e) => Err(e)
 	}
 }
 
-fn export_har_files(root_folder: String) {
+fn export_har_files(root_folder: String) -> std::io::Result<()> {
 	println!("export_har_files({})", &root_folder);
 
 	let pattern = format!("{}/{}", &root_folder, "**/*.har.json");
 
 	for file in glob(&pattern).expect("Failed to read glob pattern") {
-	 match file {
-		 Ok(path) => export_har_file(path.display().to_string()),
-		 Err(e) => println!("{:?}", e)
-	 }
+		match file {
+			Ok(path) => export_har_file(path.display().to_string()),
+			Err(e) => println!("{}", e.to_string())
+		}
 	}
+
+	Ok(())
 }
 
 fn main() -> std::io::Result<()> {
